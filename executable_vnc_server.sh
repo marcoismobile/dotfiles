f26abaab63c23c12205f4bc520b02b6c838ed512
Marco <nobody@home.now>
2023-07-22 10:43:02 +0200

    Update vnc_server.sh

--- executable_vnc_server.sh
+++ executable_vnc_server.sh
@@ -16,6 +16,9 @@ swaymsg output $SCREEN_REAL disable
 
 echo "STARTING VNC ..."
 #systemd-inhibit --who="wayvnc" --mode=block --what=idle --why="Remote working"
+
+# Swap right ALT with SUPER
+export XKB_DEFAULT_OPTIONS="altwin:swap_alt_win" # "altwin:swap_lalt_lwin"
 wayvnc --output=$SCREEN_FAKE --max-fps=15 127.0.0.1 5900
 
 echo "[SWITCHING OUTPUTS] ${SCREEN_FAKE} -> ${SCREEN_REAL}"
