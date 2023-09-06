f2902de72fa6337a688fdba3993cb2abff2c7813
Marco <nobody@home.now>
2023-09-06 10:29:38 +0200

    Update vnc_server.sh

--- executable_vnc_server.sh
+++ executable_vnc_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-HEADLESS="HEADLESS-1"
-
 # SWAY ENVIRONMENT
 export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -ox sway).sock
 export WAYLAND_DISPLAY=wayland-1
+
+HEADLESS=$(swaymsg -t get_outputs | grep name | grep HEADLESS | cut -d'"' -f 4)
 
 echo "[SWITCHING OUTPUTS] -> ENABLE ${HEADLESS}"
 swaymsg output "*" disable
@@ -17,4 +17,3 @@ XKB_DEFAULT_OPTIONS="altwin:swap_lalt_lwin" wayvnc --output=$HEADLESS 127.0.0.1 
 echo "[SWITCHING OUTPUTS] -> DISABLE ${HEADLESS}"
 swaymsg output "*" enable
 swaymsg output $HEADLESS disable
-#swaymsg output $HEADLESS unplug
